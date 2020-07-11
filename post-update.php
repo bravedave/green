1124c10a68fc4b73fc07b9a96f162c18486a2b35
Dave Bray <david@brayworth.com.au>
2020-07-11 16:51:38 +1000

    Post Install Scriptings

--- post-update.php
+++ post-update.php
@@ -13,12 +13,12 @@ require "vendor/autoload.php";
 
 class updater extends dvc\service {
     protected function _upgrade() {
-        config::route_register( 'green', 'green\\controller');
-        config::route_register( 'maintenance', 'green\\maintenance');
-        config::route_register( 'beds_list', 'green\\beds_list\\controller');
-        config::route_register( 'baths', 'green\\baths\\controller');
+        // config::route_register( 'green', 'green\\controller');
+        // config::route_register( 'maintenance', 'green\\maintenance');
+        // config::route_register( 'beds_list', 'green\\beds_list\\controller');
+        // config::route_register( 'baths', 'green\\baths\\controller');
 
-        \sys::logger( sprintf('%s : %s', 'green updated', __METHOD__));
+        // \sys::logger( sprintf('%s : %s', 'green updated', __METHOD__));
         echo( sprintf('%s : %s%s', 'green updated', __METHOD__, PHP_EOL));
 
     }
